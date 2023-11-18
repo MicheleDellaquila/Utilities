1173e221bc6bc64d24c2baa5dc767a4e867f588a
MicheleDellaquila <micheledellaquila2003@gmail.com>
2023-11-18 15:20:06 +0100

    added table components + created hooks folder

---
+++ components/table/table.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './table.css';
+
+const Table = () => {
+  return (
+    <div className='Table'>
+      <table className='Table__table'>
+        <thead>
+          <tr>{/* header table */}</tr>
+        </thead>
+        <tbody>{/* body table */}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
